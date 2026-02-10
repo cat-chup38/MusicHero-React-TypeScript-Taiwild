e3c8dffb543583ff98c3d09452b1ec7927d3d435
Adder Orozco <desarrollo08@ciancoders.dev>
2026-02-09 22:39:24 -0600

    Feature: Actualizados componentes principales.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { MainLayout } from './layout/MainLayout';
+import { SearchPage } from './pages/SearchPage';
+import { CreatePlaylistPage } from './pages/CreatePlaylistPage';
+import { MyPlaylistsPage } from './pages/MyPlaylistsPage';
 
 
 function App() {
     return (
-    <>
-        App
-    </>
-  )
+        <BrowserRouter>
+            <Routes>
+                {/* El Layout envuelve a las demás rutas como "children" */}
+                <Route path="/" element={<MainLayout />}>
+                {/* Index indica que esta es la ruta por defecto cuando entras a "/" */}
+                <Route index element={<SearchPage />} />
+                <Route path="create-playlist" element={<CreatePlaylistPage />} />
+                <Route path="my-playlists" element={<MyPlaylistsPage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux'; // El componente que "distribuye" el estado
+import { store } from './store';      // El store que configuramos antes
+import App from './App';
+import './index.css';
 
-
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        {/* El Provider debe envolver a <App /> para que CUALQUIER componente
+            dentro de la jerarquía (rutas, páginas, botones) tenga acceso
+            al estado global de Redux.
+        */}
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>
+);
